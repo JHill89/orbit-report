d343dbda7e8bc5a2f0906eb480bd2ffc4a74cf76
Joni Hill <joni.hill@rocketmail.com>
2019-11-02 18:08:21 -0500

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,18 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
+  displayList: Satellite[];
   
   constructor() {
     this.sourceList = [];
+    this.displayList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json'
 
     window.fetch(satellitesUrl).then(function(response) {
       response.json().then(function(data) {
 
         let fetchedSatellites = data.satellites;
-        // TODO: loop over satellites
-         // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
-         // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
+        
         for(let i=0; i < fetchedSatellites.length; i++) {
           let satellite = new Satellite(
             fetchedSatellites[i].name,
@@ -31,10 +31,22 @@ export class AppComponent {
           );
           this.sourceList.push(satellite);
         }
-        
+        this.displayList = this.sourceList.slice(0);
       }.bind(this));
     }.bind(this));
 
+  }
+  search(searchTerm: string): void {
+    console.log(searchTerm);
+    let matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLowerCase();
+    for(let i = 0; i < this.sourceList.length; i++) {
+      let name = this.sourceList[i].name.toLowerCase();
+      if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+      }
+    }
+    this.displayList = matchingSatellites;
   }
 
 }
--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -3,12 +3,23 @@ export class Satellite {
     orbitType: string;
     type: string;
     operational: boolean;
-    launchDate: string;
+    launchDate: string; 
+
+    shouldShowWarning():boolean {
+        // TODO read ang part 3 to figure out step 6
+        //Is line 17 accessing what type is set to in the HTML file??? or just reading that type is set to type from the constructor???
+        if(this.type.toLowerCase() === "space debris") {
+            return true;
+        } else {
+            return false;
+        }
+    }
  constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
      this.name = name,
      this.orbitType = orbitType,
      this.type = type,
      this.operational = operational,
      this.launchDate = launchDate;
+ 
  }
 }
